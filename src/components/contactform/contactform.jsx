5d441fddbff395eba7c7aeadddf8b88cd118d92b
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-07-23 15:18:44 +0300

    update

--- src/components/contactform/contactform.jsx
+++ src/components/contactform/contactform.jsx
@@ -46,7 +46,7 @@ title="Name may contain only letters, apostrophe, dash and spaces. For example A
                             value={number}
                             name="number"
                             onChange={this.handleChange}
-  pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  pattern="\\+?\\d{1,4}?[ .\\-\\s]?\\(?\\d{1,3}?\\)?[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,4}[ .\\-\\s]?\\d{1,9}"
 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
 />
